a90cd0339b5126ce511102e062e5b1a9ec18d135
brainstormjitesh <brainstorm.jitesh@gmail.com>
2018-07-27 11:44:46 +0530

    Finished our BrainTechFramwork

---
+++ BrainTechFrameWork/BrainTechFrameWork/BrainTechFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  BrainTechFrameWork.h
+//  BrainTechFrameWork
+//
+//  Created by Brainstorm Jitesh on 27/07/18.
+//  Copyright © 2018 Brainstorm Jitesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BrainTechFrameWork.
+FOUNDATION_EXPORT double BrainTechFrameWorkVersionNumber;
+
+//! Project version string for BrainTechFrameWork.
+FOUNDATION_EXPORT const unsigned char BrainTechFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrainTechFrameWork/PublicHeader.h>
+
+
---
+++ BrainTechFrameWork/BrainTechFrameWork/TestString.swift
@@ -0,0 +1,21 @@
+//
+//  TestString.swift
+//  BrainTechFrameWork
+//
+//  Created by Brainstorm Jitesh on 27/07/18.
+//  Copyright © 2018 Brainstorm Jitesh. All rights reserved.
+//
+
+import Foundation
+
+public class TestString 
+{
+    private init()
+    {
+        
+    }
+    public static func doSomething() -> String
+    {
+        return "This is first Brain Tech Framwork"
+    }
+}
